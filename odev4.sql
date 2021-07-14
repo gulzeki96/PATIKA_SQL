34d381c25574248af197b8d79cbf3a4cb6d08a89
gulzeki96 <80572516+gulzeki96@users.noreply.github.com>
2021-07-14 23:37:50 +0300

    Update odev4.sql

--- odev4.sql
+++ odev4.sql
@@ -1,5 +1,5 @@
 --SELECT DISTINCT replacement_cost FROM film;
 --SELECT COUNT(DISTINCT replacement_cost) FROM film;
---SELECT COUNT(*) FROM film WHERE title = 'T%' and rating = 'G';
+--SELECT COUNT(*) FROM film WHERE title LIKE 'T%' and rating = 'G';
 --SELECT COUNT(*) FROM country WHERE LENGTH(country) = 5;
 --SELECT COUNT(*) FROM city WHERE city ILIKE '%r';
