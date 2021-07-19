a2dce7507db9f41f11e483e4f38c254b546c7808
gulzeki96 <80572516+gulzeki96@users.noreply.github.com>
2021-07-19 15:52:32 +0300

    Update odeva10.sql

--- odeva10.sql
+++ odeva10.sql
@@ -1,3 +1,3 @@
---SELECT city.city, country, FROM city LEFT JOIN country ON country.id = city.country_id;
---SELECT payment.id, first_name, last_name, FROM payment RIGHT JOIN customer ON customer.id = payment.customer_id;
---SELECT rental.id, first_name, last_name, FROM rental FULL JOIN customer ON customer.id = rental.customer_id;
+--SELECT city.city, country, FROM city LEFT JOIN country ON country.country_id = city.country_id;
+--SELECT payment.payment_id, first_name, last_name, FROM payment RIGHT JOIN customer ON customer.customer_id = payment.customer_id;
+--SELECT rental.rental_id, first_name, last_name, FROM rental FULL JOIN customer ON customer.customer_id = rental.customer_id;
