f8f4fc473e51223cfecbfad9e59a911f029e944d
gulzeki96 <80572516+gulzeki96@users.noreply.github.com>
2021-07-19 16:27:25 +0300

    Update odeva12.sql

--- odeva12.sql
+++ odeva12.sql
@@ -1,4 +1,8 @@
 --SELECT COUNT(*) FROM film WHERE length > (SELECT AVR(length) FROM film);
 --SELECT COUNT(*) FROM film WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
 --SELECT * FROM film WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) OR replacement_cost  = (SELECT MIN(replacement_cost ) FROM film);
---SELECT payment.customer_id,customer.first_name, customer.last_name FROM customer INNER JOIN payment ON customer.id = payment.customer_id WHERE payment.amount = (SELECT MAX(amount) FROM payment);
+  
+  --SELECT SUM(payment.amount), customer.first_name, customer.last_name FROM customer 
+  --INNER JOIN payment ON customer.id = payment.customer_id 
+  --GROUP BY payment.customeid, customer.first_name, customer.last_name FROM customer 
+  --ORDER BY SUM(payment.amount) DESC LIMIT 10;
